52c693eb029e63d16fe3c875503f507e9eb4ac59
SmokingGunz <ericowens08@gmail.com>
2022-12-31 10:04:41 -0800

    v1

--- src/com/coderscampus/UserService.java
+++ src/com/coderscampus/UserService.java
@@ -1,5 +1,10 @@
 package com.coderscampus;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 //User information 
 //This will hold my buffered reader to read my external file and my array of users
 
@@ -11,5 +16,39 @@ public class UserService {
 		user.setPassword(password);
 		user.setName(name);
 		return user;
+	}
+
+	public User[] getUserArray() {
+		User[] users = new User[4];
+
+		BufferedReader fileReader = null;
+		try {
+
+			fileReader = new BufferedReader(new FileReader("user.data.txt"));
+			String line;
+
+			int i = 0;
+			while ((line = fileReader.readLine()) != null) {
+				String[] userData = line.split(",");
+				String username = userData[0];
+				String password = userData[1];
+				String name = userData[2];
+
+				users[i] = createUser(username, password, name);
+				i++;
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				fileReader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return users;
 	}
 }
