74c150242b6b846927f41bf852d47fcb576d0f60
Alex M <116844276+amamolo9986@users.noreply.github.com>
2023-01-02 15:10:13 -0600

    Update UserService.java

--- src/com/coderscampus/UserService.java
+++ src/com/coderscampus/UserService.java
@@ -1,8 +1,5 @@
 package com.coderscampus;
 
-//User information 
-//This will hold my buffered reader to read my external file and my array of users
-
 public class UserService {
 
 	public User createUser(String username, String password, String name) {
